8383d9aec8b6cf2f6814ddea661ec38fbcc4f275
Bowen <b472zhan@uwaterloo.ca>
2026-02-24 18:28:05 -0500

    Q1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,23 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        # btrual force 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if target ==  nums[j] + nums[i]:
+                    return [i,j]
+        '''
+        tmp_hash = {}
+        for i in range(len(nums)):
+            tmp_hash.setdefault(nums[i],i)
+            
+        for i in range(len(nums)):
+            if target - nums[i] in tmp_hash:
+                if i != tmp_hash.get(target - nums[i]):
+                    return [i,tmp_hash.get(target - nums[i])]
+        
+                
         
 # @lc code=end
 
